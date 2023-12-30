8c2eb03aea869552039a9d07857d5e03dc8f20b6
Jonti Talukdar <jonti.talukdar.05@gmail.com>
2023-12-30 13:30:26 -0500

    New build with headless vbox

---
+++ build/lib/pyverilog/ast_code_generator/touch.py
@@ -0,0 +1,11 @@
+import os
+import sys
+import subprocess
+
+text = open('list_ast.txt', 'r').read()
+lines = text.split('\n')
+for line in lines:
+    if line != '':
+        subprocess.call('touch template/' + line.lower() + '.txt', shell=True)
+
+#Test commit by JT with updated user permission 
